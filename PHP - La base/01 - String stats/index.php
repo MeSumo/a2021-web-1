8bf397e73cb1bc0e7b1068c6ba8823519b3cb594
Frédéric Thériault <fred.theriault@gmail.com>
2021-08-31 12:23:38 -0400

    Done with stats exercice

---
+++ PHP - La base/01 - String stats/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>String stats</title>
+        <meta charset="utf-8" />
+		<link href="css/global.css" rel="stylesheet" />
+	</head>
+	<body>
+		<div>
+			<div id="analyze-icon">
+				
+			</div>
+			<div id="analyze-result">
+				<?php
+					if (!empty($_GET["searchBox"])) {
+						echo strlen($_GET["searchBox"]);
+					}
+					else {
+						?>
+						Entrez une chaîne de caractères et appuyez sur analyser						
+						<?php
+					}
+				?>
+			</div>
+			<div id="search-line">
+				<form action="" method="get">
+					<div>
+						<input type="text" name="searchBox" placeholder="Texte à analyser" />
+					</div>
+					<div class="btn">
+						<button>Analyser</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
